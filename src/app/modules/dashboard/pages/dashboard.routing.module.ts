2c1169d5481e7d22f3dbc391805763cf5ee6954c
Sara <costanzosara96@gmail.com>
2022-03-24 17:18:20 +0100

    commento sec guard

--- src/app/modules/dashboard/pages/dashboard.routing.module.ts
+++ src/app/modules/dashboard/pages/dashboard.routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './home/home.component';
 
 
@@ -8,6 +7,7 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+    // canActivate:[AuthenticationGuard]
   }
 ];
 
